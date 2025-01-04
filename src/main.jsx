7f6e19b5a0807585204279acd50cc39e3086a03a
Hembram-G <goutamkumarhembram02@gmail.com>
2025-01-05 00:49:45 +0530

    configures auto import sorting for eslint

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
-import { createRoot } from 'react-dom/client'
+// Component imports
 import './index.css'
+
+//Library IMPORTS
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+
+//CSS imports
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
+   
 );
