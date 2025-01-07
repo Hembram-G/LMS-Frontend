d6dfc930b59713bca08373db51d848ab5e958349
Hembram-G <goutamkumarhembram02@gmail.com>
2025-01-08 00:22:20 +0530

    Cleaned up our about us

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,39 @@
+import apj from "../Assets/images/apj.png";
+import billGates from "../Assets/images/billGates.png";
+import einstein from "../Assets/images/einstein.png";
+import nelsonMandela from "../Assets/images/nelsonMandela.png";
+import steveJobs from "../Assets/images/steveJobs.png"
+
+export const celebrities = [
+    {
+        titel: "Steve Jobs",
+        description: "Innovation distinguishes between a leader and a follower.",
+        image:steveJobs,
+        slideNumber:1 
+    },
+    {
+        titel: "Bill Gates",
+        description: "Don’t compare yourself with anyone in this world. If you do so, you are insulting yourself.",
+        image:billGates,
+        slideNumber:2 
+    },
+    {
+        titel: "A.P.J. Abdul Kalam",
+        description: "The best brains of the nation may be found on the last benches of the classroom.",
+        image:apj,
+        slideNumber:3 
+    },
+    {
+        titel: "Albert Eistein",
+        description: "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world.",
+        image:einstein,
+        slideNumber:4 
+    },
+    {
+        titel: "Nelson Mandela",
+        description:"Education is the most powerful weapon which you can use to change the world.",
+        image:nelsonMandela,
+        slideNumber:5 
+    },
+
+]
